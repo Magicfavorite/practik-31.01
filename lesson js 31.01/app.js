78488e8d01df0e92962880c54ef659116f31c65f
Magicfavorite <131023322+Magicfavorite@users.noreply.github.com>
2024-01-31 20:36:41 +0300

    Add files via upload

---
+++ lesson js 31.01/app.js
@@ -0,0 +1,71 @@
+
+
+// if(age > 30){
+//     console.log("Вам больше 30ти, пора задуматься");
+// }
+// else if(age == 30) {
+// console.log("Вам ровно 30 лет");
+
+// }
+// else {
+//     console.log("Вам меньше 30ти, радуйтесь");
+// }
+
+
+// let age = 22;
+// userLet = age > 28 ? "Доступ разрешён. Вам 18 лет" :
+// age == 18 ? "Доступ разрешён. Вам 18 лет" :
+// "Доступ запрещён. Вам меньше 18"
+// console.log(userLet)
+
+
+
+// let DayOfWeek = "Tuesday"
+// switch(DayOfWeek){
+//     case "Sunday":
+//         console.log("Сегодня воскресенье");
+//         break;
+//     case "Monday":
+//         console.log("Сегодня понедельник");
+//         break;
+//     case "Tuesday":
+//         console.log("Сегодня вторник");
+//         break
+//     case "Wednesday":
+//         console.log("Сегодня среда");
+//         break;
+// }
+
+
+// for(let i = 0; i < 12; i++)(
+//     console.log(i)
+// )
+
+// const my_arr = ['a','b','c','d'];
+// for (let i = 0; i < my_arr.length; i++){
+//     console.log(my_arr[i]);
+// }
+
+// for (i in my_arr){
+//     console.log(i);
+
+// }
+
+
+// for (i of my_arr){
+//     console.log(i);
+    
+// }
+
+// let x = 5;
+
+// while (x < 5){
+//     console.log(x)
+//     x++
+// }
+
+// let y = 5;
+// do{
+//     console.log(y)
+//     y++
+// } while (y < 5)
